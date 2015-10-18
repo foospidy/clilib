b51ef66a5bf29fe8a10bd8a3901c48e6875d5627
foospidy <foospidy@users.noreply.github.com>
2015-10-18 19:34:34 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.rst')) as f:
+    long_desc = f.read()
+
+setup(
+    name = "clilib",
+    version = "0.0.0",
+    author = "foospidy",
+    author_email = "",
+    description = ("Emulates command line commands"),
+    license = "GPLv2",
+    keywords = "unix windows cli command line",
+    url = "https://github.com/foospidy/clilib",
+    download_url = "",
+    packages = ['clilib'],
+    long_description = long_desc,
+    classifiers = [
+        "Intended Audience :: Honeypots",
+        "Topic :: Unix Windows Honeypots :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 2.7",
+        "License :: GPLv2 License",
+    ],
+    install_requires = ['']
+)
